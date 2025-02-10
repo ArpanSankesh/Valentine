4c279f1210a5ed611983668fc2db4dafaed8f4b4
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-11 02:22:24 +0530

    logic

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let img = document.querySelector('img');
 let yesBtn = document.querySelector('#yes')
 let noBtn = document.querySelector('#no')
+let txt = document.querySelector('.question h1')
+let buttons = document.querySelector('.button-div')
 
 let images = [
     "assets/SadPikachu1.webp",
@@ -8,15 +10,32 @@ let images = [
     "assets/SadPikachu3.webp",
     "assets/SadPikachu4.webp",
     "assets/SadPikachu5.webp",
-    "assets/SadPikachu6.webp"
 ];
+const messages = ["really?", "pookie please", "are you sure?", "Last chance!", "Think again"];
+
 
 let index = 0 
+let scaleFactor = 1
 let changeImg = () => {
-    console.log("changed");
     
-    img.src = [images]
-    images = (index + 1 ) % images.length
+    img.src = images[index]
+    index = (index + 1) % images.length
+
+    noBtn.innerHTML = `${messages[index]}`
+    index = (index + 1) % images.length
+
+    scaleFactor += 0.2
+    yesBtn.style.transform = `scale(${scaleFactor})`;
+    console.log(scaleFactor);
+    
+}
+
+let congrats = () => {
+    buttons.style.display = 'none'
+    txt.innerHTML = "Yeahhh... Thank you :)"
+    img.src = 'assets/happyPika.webp'
 }
 
 noBtn.addEventListener('click', changeImg)
+yesBtn.addEventListener('click', congrats)
+
