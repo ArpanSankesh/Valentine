0eb9d32f6e360b458fc5fedddf32b307fc52abe4
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-11 09:22:35 +0530

    bug fix

--- script.js
+++ script.js
@@ -14,31 +14,10 @@ let images = [
 const messages = ["really?", "pookie please", "are you sure?", "Last chance!", "Think again"];
 const yesMessages = ["Come On", "Say Yes", "SAYY YESS" , "SAYYY YESSS", "SAYYYY YESSSS"];
 
-window.addEventListener('beforeunload', function (e) {
-    e.preventDefault();
-    e.returnValue = '';  // This triggers the "Are you sure?" prompt
-});
-
-// Function to make the page go fullscreen
-function goFullScreen() {
-    let elem = document.documentElement;
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { // Firefox
-        elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { // IE/Edge
-        elem.msRequestFullscreen();
-    }
-}
-
 
 let index = 0 
 let scaleFactor = 1
 let changeImg = () => {
-
-    goFullScreen();
     
     img.src = images[index]
     index = (index + 1) % images.length
@@ -54,7 +33,6 @@ let changeImg = () => {
 }
 
 let congrats = () => {
-    window.removeEventListener('beforeunload', () => {});
     buttons.style.display = 'none'
     txt.innerHTML = "Pika Pika! You made Pikachu happy! 💖"
     img.src = 'assets/happyPika.webp'
